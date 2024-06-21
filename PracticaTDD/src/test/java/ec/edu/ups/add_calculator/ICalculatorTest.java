94f0739b06d15b46ce5a3972c5796e48be07c712
Paulguzhnay <69018647+Paulguzhnay@users.noreply.github.com>
2024-06-21 16:25:57 -0500

    Actualizacion de Pruebas

--- PracticaTDD/src/test/java/ec/edu/ups/add_calculator/ICalculatorTest.java
+++ PracticaTDD/src/test/java/ec/edu/ups/add_calculator/ICalculatorTest.java
@@ -15,6 +15,12 @@ public class ICalculatorTest {
         Mockito.when(c.addition(3,2)).thenReturn(5);
         assertEquals(5,c.addition(3,2));
     }
+    @Test
+    public void give_two_integers_when_addition_then_error() {
+        ICalculator c = Mockito.mock(ICalculator.class);
+        Mockito.when(c.addition(3,2)).thenReturn(5);
+        assertEquals(5,c.addition(2,3));
+    }
 
     //SUBTRACTION
     @Test
@@ -24,12 +30,24 @@ public class ICalculatorTest {
         assertEquals(5,c.subtraction(10,5));
     }
 
+    @Test
+    public void give_two_integers_when_subtraction_then_error() {
+        ICalculator c = Mockito.mock(ICalculator.class);
+        Mockito.when(c.subtraction(10,5)).thenReturn(5);
+        assertEquals(5,c.subtraction(5,10));
+    }
     //MULTIPLICATION
     @Test
     public void give_two_integers_when_multiplication_then_ok() {
         ICalculator c = Mockito.mock(ICalculator.class);
         Mockito.when(c.multiplication(5,10)).thenReturn(50);
         assertEquals(50,c.multiplication(5,10));
+    }
+    @Test
+    public void give_two_integers_when_multiplication_then_error() {
+        ICalculator c = Mockito.mock(ICalculator.class);
+        Mockito.when(c.multiplication(5,10)).thenReturn(50);
+        assertEquals(50,c.multiplication(10,5));
     }
 
 
@@ -39,6 +57,12 @@ public class ICalculatorTest {
         ICalculator c = Mockito.mock(ICalculator.class);
         Mockito.when(c.division(20,2)).thenReturn(10);
         assertEquals(10,c.division(20,2));
+    }
+    @Test(expected = ArithmeticException.class)
+    public void give_two_integers_when_division_then_error() {
+        ICalculator c = Mockito.mock(ICalculator.class);
+        Mockito.when(c.division(20,2)).thenReturn(10);
+        assertEquals(10,c.division(20,0));
     }
 
 }
